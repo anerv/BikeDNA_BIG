4c9a89b8615a49faa7fc5120902f7ddab0ffdab0
Ane Rahbek Vierø <vieroe@hotmail.com>
2023-03-14 11:50:09 +0100

    reran, fixed bug

--- scripts/settings/load_refdata.py
+++ scripts/settings/load_refdata.py
@@ -18,28 +18,30 @@ ref_graph_simplified = ox.load_graphml(
 print("Reference graphs loaded successfully!")
 
 # Load grid
-ref_grid = gpd.read_file(ref_grid_fp)
+ref_grid = gpd.read_parquet(ref_grid_fp)
 grid_ids = ref_grid.grid_id.to_list()
 
 # # Load saved edged and nodes
-ref_nodes = gpd.read_file(ref_nodes_fp)
-ref_nodes.set_index("osmid", inplace=True)
+ref_nodes = gpd.read_parquet(ref_nodes_fp)
+#ref_nodes.set_index("osmid", inplace=True)
+assert ref_nodes.index.name == 'osmid'
 
-ref_edges = gpd.read_file(ref_edges_fp)
-ref_edges.set_index(["u", "v", "key"], inplace=True)
+ref_edges = gpd.read_parquet(ref_edges_fp)
+#ref_edges.set_index(["u", "v", "key"], inplace=True)
 
-ref_edges_simplified = gpd.read_file(ref_edges_simplified_fp)
-ref_edges_simplified.set_index(["u", "v", "key"], inplace=True)
+ref_edges_simplified = gpd.read_parquet(ref_edges_simplified_fp)
+#ref_edges_simplified.set_index(["u", "v", "key"], inplace=True)
 
-ref_nodes_simplified = gpd.read_file(ref_nodes_simplified_fp)
-ref_nodes_simplified.set_index("osmid", inplace=True)
+ref_nodes_simplified = gpd.read_parquet(ref_nodes_simplified_fp)
+#ref_nodes_simplified.set_index("osmid", inplace=True)
+assert ref_nodes_simplified.index.name == 'osmid'
 
-ref_edges_joined = gpd.read_file(ref_edges_joined_fp)
+ref_edges_joined = gpd.read_parquet(ref_edges_joined_fp)
 
-ref_nodes_joined = gpd.read_file(ref_nodes_joined_fp)
+ref_nodes_joined = gpd.read_parquet(ref_nodes_joined_fp)
 
-ref_edges_simp_joined = gpd.read_file(ref_edges_simplified_joined_fp)
+ref_edges_simp_joined = gpd.read_parquet(ref_edges_simplified_joined_fp)
 
-ref_nodes_simp_joined = gpd.read_file(ref_nodes_simplified_joined_fp)
+ref_nodes_simp_joined = gpd.read_parquet(ref_nodes_simplified_joined_fp)
 
 print("Reference data loaded successfully!")
